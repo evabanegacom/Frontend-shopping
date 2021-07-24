6258ed629fe6e0639a885bdf39cbbfef059582b2
Precious chiedu <udegbue69@gmail.com>
2021-07-24 20:32:30 +0100

    added hompage category links

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from "react";
 import { autoLogin, getProducts } from "../actions/actions";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
 import "./home.css";
 import electronic from '../images/electronic.png';
 import ladyImage from '../images/slazzer-edit.png';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { Button, TextField, Typography, Paper, Box } from '@material-ui/core';
 
 const Home = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const products = useSelector((state) => state.products.products);
-
+  
+  const sliced = products.length && products.filter((product) => product.category === 'BestDeals')
 
   useEffect(() => {
     dispatch(autoLogin());
@@ -46,7 +47,7 @@ const Home = () => {
             products.map((product) => {
               return product.category === 'BestDeals' && (
                
-                <div className='carouselItem'>
+                <div key={product.id} className='carouselItem'>
                 <img src={product.avatar.url.replace(/http/g, "https")} alt='' />
                 
                 </div>
@@ -59,11 +60,29 @@ const Home = () => {
         </section>
       <div className='homeContent'>
         <div className="caption">
-          <img src={ladyImage} alt='' />
+          <img src={ladyImage} alt='lady' />
         </div>
         <div className="forImage">
-          <img src={electronic} alt='' />
+          <img src={electronic} alt='pc' />
         </div>
+      </div>
+      <div className='labels'>
+        Home Theatres
+      </div>
+      <div className='homeTheatre'>
+      {sliced && sliced.length && (
+            sliced.slice(0, Number(4)).map((product) => 
+               (
+               <Paper elevation={10} className='homeTheatreItem'>
+                <div key={product.id}>
+                <p>{product.name}</p>
+                <img src={product.avatar.url.replace(/http/g, "https")} alt='' />
+                <p className='categoryText'><Button component={Link} to='/products' variant='contained' color="primary">See More</Button></p>
+                </div>
+                </Paper>
+              )
+            )
+          )}
       </div>
     </div>
   );
