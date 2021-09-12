be545e8d0067fee742ed219170d3c10206157c68
Precious chiedu <udegbue69@gmail.com>
2021-09-12 19:33:14 +0100

    worked on 404 error page

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
+import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core';
 
 const PageNotFound = () => {
     const NotFound = styled.div`
-      ${'' /* background: url(${'https://cdn4.vectorstock.com/i/1000x1000/75/83/404-error-page-not-found-plug-graphic-vector-19997583.jpg'});
-      background-size: auto auto;
-      background-repeat: no-repeat; */}
-      
-      img {
-          @media only screen and (max-width: 767px){
-              width: 300px;
-              heght: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 40px 0;
+    img{
+          height: 300px;
+          width: 400px
+      }
+      @media only screen and (max-width: 767px){
+          img{
+              width: 320px;
           }
       }
     `;
     return (
         <NotFound>
-            <p>404 PAGE NOT FOUND</p>
-            <img style={{width: '100%'}} src='https://cdn4.vectorstock.com/i/1000x1000/75/83/404-error-page-not-found-plug-graphic-vector-19997583.jpg' alt='' />
+            <div style={{ background: 'white', width: '100%'}}><Button component={Link} to='/'>Go to homepage</Button></div>
+            <div style={{ background: '#131c3b', width: '100%'}}><img src='https://res.cloudinary.com/spetsnaz/image/upload/v1631470398/screenshot-web.whatsapp.com-2021.09.12-19_09_11_xdfrsr.png' alt='' /></div>
         </NotFound>
     )
 }
--- src/containers/newProductPage.js
+++ src/containers/newProductPage.js
@@ -3,6 +3,7 @@ import { getProducts } from '../actions/actions';
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from '@material-ui/core';
 import { addToCart } from '../actions/actions';
+import styled from "styled-components";
 import 'react-notifications/lib/notifications.css';
 import {
    NotificationContainer,
@@ -26,17 +27,22 @@ const NewProductPage = (props) => {
         (product) => product.id === parseInt(props.match.params.id, 10)
       );
 
+      const ProductDiv = styled.div`
+        display: flex;
+        align-items: center;
+      `;
+
     return (
         <>
         { filterProduct.length && filterProduct.map((product) =>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '30px', marginBottom: '30px'}}>
+          <ProductDiv>
               <div><p>{product.name}</p></div>
-              <div style={{ marginTop: '20px'}}><p>{product.price}</p></div>
-              <div style={{ marginTop: '20px', width: '100%', display: 'flex', justifyContent: 'center'}}><img xs={12} sm={6} md={4} lg={3} src={product.avatar.url.replace(/http/g, "https")} alt={product.name} /></div>
-              <div style={{ marginTop: '20px'}}><p>{product.description}</p></div>
-              <Button style={{background: 'green', color: '#fff'}} onClick={() => handleAdd(product)} >Buy Now</Button>
+              <div><p>{product.price}</p></div>
+              <div><img src={product.avatar.url.replace(/http/g, "https")} alt={product.name} /></div>
+              <div><p>{product.description}</p></div>
+              <Button onClick={() => handleAdd(product)} >Buy Now</Button>
               <NotificationContainer />
-          </div>
+          </ProductDiv>
         )}
         </>
     )
