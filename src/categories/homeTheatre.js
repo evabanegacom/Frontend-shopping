769e522f1970efda34458e7aea8693b1d91a6db2
Precious chiedu <udegbue69@gmail.com>
2021-10-03 21:55:52 +0100

    removed redundant code

--- src/categories/homeTheatre.js
+++ src/categories/homeTheatre.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts, addToCart } from '../actions/actions';
-import { Box, Grid, Button, Menu, MenuItem } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import Product from '../cart/product';
 import useStyles from '../containers/styles';
 import ReactPaginate from "react-paginate";
@@ -10,6 +10,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import './sliderCategory.css';
+import SecondSlider from './sliders';
 import 'react-notifications/lib/notifications.css';
 import {
    NotificationContainer,
@@ -21,47 +22,6 @@ const HomeTheatre = () => {
 
   const classes = useStyles();
 
-  useEffect(() => {
-      dispatch(getProducts())
-    }, [])
-
-    const products = useSelector((state) => state.products.products);
-
-    var settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      slidesToShow: 4,
-      slidesToScroll: 1
-    };
-
-    var smallSettings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-
-    var mediumSettings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      slidesToShow: 3,
-      slidesToScroll: 1
-    };
-
-    var smallerSettings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      slidesToShow: 2,
-      slidesToScroll: 1
-    };
     
     const [ data, setData] = useState([]);
 
@@ -79,7 +39,7 @@ const HomeTheatre = () => {
       }))
     }, []);
     
-  const sliced = data.length && data.filter((product) => product.category === 'Home-Theatres')
+   const sliced = data.length && data.filter((product) => product.category === 'Home-Theatres')
 
   const [currentPage, setCurrentPage ] = useState(0)
   const [search, setSearch ] = useState('')
@@ -127,62 +87,7 @@ const HomeTheatre = () => {
           
         /></div>
         </div>
-         <section className='bigSlider'>
-        <Slider className='sliderMain' {...settings}>
-          {products && products.length && (
-            products.map((product) => (
-              <Grid container justify="center" spacing={2}>
-                
-                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
-                
-          </Grid>
-          ))
-          )}
-          </Slider>
-        </section>
-
-        <section className='mediumSlider'>
-        <Slider className='small' {...mediumSettings}>
-          {products && products.length && (
-            products.map((product) => (
-              <Grid container justify="center" spacing={2}>
-                
-                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
-                
-          </Grid>
-          ))
-          )}
-          </Slider>
-        </section>
-
-        <section className='smallerSlider'>
-        <Slider className='small' {...smallerSettings}>
-          {products && products.length && (
-            products.map((product) => (
-              <Grid container justify="center" spacing={2}>
-                
-                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
-                
-          </Grid>
-          ))
-          )}
-          </Slider>
-        </section>
-
-        <section className='smallSlider'>
-        <Slider className='small' {...smallSettings}>
-          {products && products.length && (
-            products.map((product) => (
-              <Grid container justify="center">
-                
-                <Grid item key={Product.id} xs={12} sm={9} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
-                
-          </Grid>
-          ))
-          )}
-          </Slider>
-        </section>
-
+<SecondSlider />
             <main className={classes.content}>
       <Grid container justify="center" spacing={4}>
                 {sliced && sliced.length ? (
@@ -215,4 +120,4 @@ const HomeTheatre = () => {
     )
 }
 
-export default HomeTheatre
+export default HomeTheatre;
---
+++ src/categories/sliders.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts, addToCart } from '../actions/actions';
+import { Box, Grid } from '@material-ui/core';
+import Product from '../cart/product';
+import useStyles from '../containers/styles';
+import ReactPaginate from "react-paginate";
+import Filter from './filterCategories';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import './sliderCategory.css';
+import 'react-notifications/lib/notifications.css';
+import {
+   NotificationContainer,
+   NotificationManager,
+ } from 'react-notifications';
+
+const Sliders = () => {
+  const dispatch = useDispatch()
+
+  const classes = useStyles();
+
+  useEffect(() => {
+      dispatch(getProducts())
+    }, [])
+
+    const products = useSelector((state) => state.products.products);
+
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 4,
+      slidesToScroll: 1
+    };
+
+    var smallSettings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
+    var mediumSettings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    };
+
+    var smallerSettings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 2,
+      slidesToScroll: 1
+    };
+    
+    const [ data, setData] = useState([]);
+
+    useEffect (function effectFunction() {
+      fetch('https://neptune-spear.herokuapp.com/api/v1/products', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          // Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then(response => response.json())
+      .then((data => {
+        setData(data)
+      }))
+    }, []);
+    
+  
+    return (
+        <>
+        
+        
+         <section className='bigSlider'>
+        <Slider className='sliderMain' {...settings}>
+          {products && products.length && (
+            products.map((product) => (
+              <Grid container justify="center" spacing={2}>
+                
+                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
+                
+          </Grid>
+          ))
+          )}
+          </Slider>
+        </section>
+
+        <section className='mediumSlider'>
+        <Slider className='small' {...mediumSettings}>
+          {products && products.length && (
+            products.map((product) => (
+              <Grid container justify="center" spacing={2}>
+                
+                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
+                
+          </Grid>
+          ))
+          )}
+          </Slider>
+        </section>
+
+        <section className='smallerSlider'>
+        <Slider className='small' {...smallerSettings}>
+          {products && products.length && (
+            products.map((product) => (
+              <Grid container justify="center" spacing={2}>
+                
+                <Grid item key={Product.id} xs={12} sm={12} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
+                
+          </Grid>
+          ))
+          )}
+          </Slider>
+        </section>
+
+        <section className='smallSlider'>
+        <Slider className='small' {...smallSettings}>
+          {products && products.length && (
+            products.map((product) => (
+              <Grid container justify="center">
+                
+                <Grid item key={Product.id} xs={12} sm={9} md={12} lg={12}><Product product={product} key={product.id} /></Grid>
+                
+          </Grid>
+          ))
+          )}
+          </Slider>
+        </section>
+
+            
+        </>
+    )
+}
+
+//const sliced = data.length && data.filter((product) => product.category === 'Television-sets')
+
+export default Sliders
